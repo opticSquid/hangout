ed22ded9d4b8f5e857f54aa7bda59be6c930d517
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-09-04 21:37:08 +0530

    adding scripts to deploy postgres, kafka in kubernetes

---
+++ k8-configs/kafka/start-kafka.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka > start-kafka-output.log
---
+++ k8-configs/postgres/start-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl apply -f k8s-postgres-pv.yaml
+kubectl apply -f k8s-postgres-pvc.yaml
+kubectl apply -f k8s-postgres-secret.yaml
+kubectl apply -f k8s-postgres-deployment.yaml
+kubectl apply -f k8s-postgres-service.yaml
