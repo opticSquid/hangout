305f57cb34c5f568cf4f638699bc7c3a3a08fd11
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-09-30 20:58:42 +0530

    publishing initial stable version of hangout-auth-service and hangout-notification-service

--- k8-configs/kafka/start-kafka.sh
+++ k8-configs/kafka/start-kafka.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 kubectl create namespace persistance
 helm install kafka --namespace persistance --values kafka.yaml oci://registry-1.docker.io/bitnamicharts/kafka > kafka.log
-kafka-ui-98b6b6b6-7z2r8
 helm repo add kafbat-ui https://kafbat.github.io/helm-charts
 helm install kafka-ui --namespace persistance --values kafka-ui.yaml kafbat-ui/kafka-ui > kafka-ui.log
