2e1e8ca63f8cbe6e14f9fd59e0ec580ad34d0784
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-04-13 10:07:09 +0530

    all infrastructure components deployed in gcp successfully

--- kubernetes/kafka/start-kafka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl create namespace persistance
-helm install kafka --namespace persistance --values kafka.yaml oci://registry-1.docker.io/bitnamicharts/kafka > kafka.log
-helm repo add kafbat-ui https://kafbat.github.io/helm-charts
-helm install kafka-ui --namespace persistance --values kafka-ui.yaml kafbat-ui/kafka-ui > kafka-ui.log
