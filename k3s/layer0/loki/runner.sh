32e901160238a9f5413c57196f01369946e2c4cd
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-08-06 13:28:29 +0530

    removing k3s directory

--- k3s/layer0/loki/runner.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm upgrade --install loki grafana/loki -f single-binary.yaml --namespace=layer0
--- k3s/layer1/grafana/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace layer1
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm upgrade --install grafana grafana/grafana --namespace layer1
-echo "password: "
-kubectl get secret --namespace layer1 grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-kubectl -n layer1 port-forward --address 0.0.0.0 svc/grafana 3000:80
